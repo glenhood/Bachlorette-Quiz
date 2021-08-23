3e4075dd2a45a0981099141880f97d4699bb42df
Glen Hood <glenhoodatx@gmail.com>
2021-08-23 16:11:02 -0500

    edited to be a bachlorette quiz

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,29 @@
 var questions = [
     {
-        question: "Inside which HTML element do we put the JavaScript?",
-        choices: ["a. <js>", "b. <javascript>", "c. <scripting>", "d. <script>"],
-        answer: "d. <script>"
+        question: "Kristin has won the lotto, what is she buying?",
+        choices: ["a. AK-47", "b. Diamond Necklace", "c. Hood Rat Stuff", "d. 4 Runner"],
+        answer: "d. 4 Runner"
     },
-    {
-        question: "String values must be enclosed within _____ when being assigned to variables.",
-        choices: ["a. commas", "b. curly brackets", "c. quotes", "d. parenthesis"],
-        answer: "c. quotes"
-    },
-    {
-        question: "Arrays in JavaScript can be used to store _____.",
-        choices: ["a. numbers and strings", "b. other arrays", "c. booleans", "d. all of the above"],
-        answer: "b. other arrays"
-    },
-    {
-        question: "Commonly used data types DO NOT include:",
-        choices: ["a. strings", "b. booleans", "c. alerts", "d. numbers"],
-        answer: "c. alerts"
-    },
-    {
-        question: "How do you create a function in JavaScript",
-        choices: ["a. function = myFunction()", "b. function myFunction()", "c. function:myFunction()", "d. createMyFunction()"],
-        answer: "b. function myFunction()"
-    },
+    // {
+    //     question: "String values must be enclosed within _____ when being assigned to variables.",
+    //     choices: ["a. commas", "b. curly brackets", "c. quotes", "d. parenthesis"],
+    //     answer: "c. quotes"
+    // },
+    // {
+    //     question: "Arrays in JavaScript can be used to store _____.",
+    //     choices: ["a. numbers and strings", "b. other arrays", "c. booleans", "d. all of the above"],
+    //     answer: "b. other arrays"
+    // },
+    // {
+    //     question: "Commonly used data types DO NOT include:",
+    //     choices: ["a. strings", "b. booleans", "c. alerts", "d. numbers"],
+    //     answer: "c. alerts"
+    // },
+    // {
+    //     question: "How do you create a function in JavaScript",
+    //     choices: ["a. function = myFunction()", "b. function myFunction()", "c. function:myFunction()", "d. createMyFunction()"],
+    //     answer: "b. function myFunction()"
+    // },
 ];
 
 var timer = document.getElementById("timer");
@@ -60,10 +60,10 @@ var scoreResult;
 var questionIndex = 0;
 
 
-var totalTime = 61;
+var totalTime = 1001;
 function newQuiz() {
     questionIndex = 0;
-    totalTime = 60;
+    totalTime = 1000;
     timeLeft.textContent = totalTime;
     initialInput.textContent = "";
 
